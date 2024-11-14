b243c33c248a9ec4e8460e51d0611d28e626a88d
Zhikun Xi <zhikun@ifarm2401.jlab.org>
2024-11-14 02:01:06 -0500

    Finishing ADC Fits

--- scriptArea/parameters.py
+++ scriptArea/parameters.py
@@ -10,3 +10,9 @@ outputDir = "./outputRootFiles/"
 # type the command you want to run herem do not include the files, you can add other parameters
 runCommand = "hd_root -PTT:XML_FILENAME=tt_RunPeriod-2025-01-default.xml -PPLUGINS=cosmicRayTestEvio "
 fileType = ".evio" #can be changed to ".hddm"
+
+#if want to run in personal folder, leave publicFileDir empty.
+#if want to run in /cache, type in the whole path that includes the folder
+# Eg. '/cache/halld/RunPeriod-2025-01/rawdata/Run130069/'
+publicFileDir = ['']
+runId = [(),]
