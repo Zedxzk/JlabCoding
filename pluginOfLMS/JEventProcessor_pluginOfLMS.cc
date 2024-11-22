a9f125d68d916b209373097d4613e271ae390997
Zhikun Xi <zhikun@ifarm2402.jlab.org>
2024-11-21 19:53:12 -0500

    Some updata

---
+++ pluginOfLMS/JEventProcessor_pluginOfLMS.cc
@@ -0,0 +1,477 @@
+// $Id$
+//
+//    File: JEventProcessor_pluginOfLMS.cc
+// Created: Thu Nov 21 07:23:30 PM EST 2024
+// Creator: zhikun (on Linux ifarm2402.jlab.org 5.14.0-427.42.1.el9_4.x86_64 x86_64)
+//
+
+#include "JEventProcessor_pluginOfLMS.h"
+using namespace jana;
+using namespace cutsConstants;
+using namespace globalVariables;
+
+const bool printAllAcceptedEventsAfterCuts = false;
+
+
+// Routine used to create our JEventProcessor
+#include <JANA/JApplication.h>
+#include <JANA/JFactory.h>
+#include <TTree.h>
+#include <TSystem.h>
+#include <set>
+
+Int_t plotIndex = 1;
+Int_t MinEcalSizeToAccept = 8;
+// Routine used to create our JEventProcessor
+#include <JANA/JApplication.h>
+#include <JANA/JFactory.h>
+extern "C"{
+void InitPlugin(JApplication *app){
+	InitJANAPlugin(app);
+	app->AddProcessor(new JEventProcessor_pluginOfLMS());
+}
+} // "C"
+
+
+//------------------
+// JEventProcessor_pluginOfLMS (Constructor)
+//------------------
+JEventProcessor_pluginOfLMS::JEventProcessor_pluginOfLMS()
+{
+
+}
+
+//------------------
+// ~JEventProcessor_pluginOfLMS (Destructor)
+//------------------
+JEventProcessor_pluginOfLMS::~JEventProcessor_pluginOfLMS()
+{
+
+}
+
+//------------------
+// init
+//------------------
+jerror_t JEventProcessor_pluginOfLMS::init(void)
+{
+	// This is called once at program startup. 
+if(printAllAcceptedEventsAfterCuts){
+		const char* dirPath = "figures";
+		// 检查目录是否存在
+		if (gSystem->AccessPathName(dirPath) == 0) {
+			std::cout << "Directory exists. Deleting all files inside..." << std::endl;
+			
+			// 使用系统命令删除目录中的所有文件
+			gSystem->Exec(Form("rm -rf %s/*", dirPath));
+			std::cout << "All files deleted inside '" << dirPath << "'." << std::endl;
+		} else {
+			std::cout << "Directory does not exist. Creating it..." << std::endl;
+		}
+
+		// 创建目录
+		gSystem->Exec(Form("mkdir -p %s", dirPath));
+		std::cout << "Directory '" << dirPath << "' is ready for use." << std::endl;
+	}
+
+
+
+	ecalHitsTree = new TTree("EcalHits", "EcalHits"       );
+	ecalHitsTree->Branch("EcalChannelNo"                              , &channelNoByColumn       );
+	ecalHitsTree->Branch("EcalEnergyDistributionByColumn"             , &energyBranchVar        );
+	ecalHitsTree->Branch("EcalTimeDistributionByColumn"               , &timeBranchVar          );
+	ecalDigitHitsTree = new TTree("EcalDigitHits", "EcalDigitHits" );
+	ecalDigitHitsTree->Branch("EcalChannelNo"                              , &digiChannelNoByColumn  );
+	ecalDigitHitsTree->Branch("EcalpulsePeakDistributionByColumn"          , &pulsePeakBranchVar     );
+	ecalDigitHitsTree->Branch("EcalpulseIntegralDistributionByColumn"      , &pulseIntegralBranchVar );
+	ecalDigitHitsTree->Branch("EcalpulseTimeDistributionByColumn"          , &pulseTimeBranchVar     );
+	// This is called once at program startup. 
+	for(int i = 0;i < 40 ; i++){
+		for(int j = 0; j < 40; j++){
+			channelMapByCol[j][i] = (39 - j) * 40 + (39 - i) + 0.5;
+			channelMapByRow[j][i] = i * 40 + j + 0.5;
+		}
+	}
+	//record start time and convert it to string
+	startTime = std::chrono::high_resolution_clock::now();
+	startTimeString = commonFunctions::getTimeString(startTime);
+	// setPlotStyleOfHits1D(EcalTimeColumnPlot1D);
+    // setPlotStyleOfHits1D(EcalEnergyColumnPlot1D);
+    // setPlotStyleOfHits1D(FcalTimeColumnPlot1D);
+    // setPlotStyleOfHits1D(FcalEnergyColumnPlot1D);
+    // setPlotStyleOfHits1D(EcalTimeRowPlot1D);
+    // setPlotStyleOfHits1D(EcalEnergyRowPlot1D);
+    // setPlotStyleOfHits1D(FcalTimeRowPlot1D);
+    // setPlotStyleOfHits1D(FcalEnergyRowPlot1D);
+
+	// initPlotStyleOfHits2D(hitsEnergy2D);
+	// initPlotStyleOfHits2D(hitsTime2D);
+	// initPlotStyleOfHits2D(hitsCount2D);
+
+	// initPlotStyleOfHits2D(rowChannelEnergy2D);
+	// initPlotStyleOfHits2D(columnChannelEnergy2D);
+	// initPlotStyleOfHits2D(rowChannelTime2D);
+	// initPlotStyleOfHits2D(columnChannelTime2D);
+	
+	// initPlotStyleOfHits2D(rowChannelDigitalPeak2D);
+	// initPlotStyleOfHits2D(columnChannelDigitalPeak2D);
+	// initPlotStyleOfHits2D(rowChannelDigitalIntegral2D);
+	// initPlotStyleOfHits2D(columnChannelDigitalIntegral2D);
+	// initPlotStyleOfHits2D(rowChannelDigitalTime2D);
+	// initPlotStyleOfHits2D(columnChannelDigitalTime2D);
+    gStyle->SetOptStat(0);
+  
+	gStyle->SetTitleFontSize(0.05);
+  
+	gStyle->SetTitleFont(130,"xy");
+	gStyle->SetLabelFont(130,"xy");
+	
+	gStyle->SetLineWidth(1.5);
+	gStyle->SetTextSize(1.2);
+	
+	
+	gStyle->SetLabelSize(0.04,"xy");
+	gStyle->SetTitleSize(0.03,"xy");
+	
+	
+	gStyle->SetTitleOffset(0.5,"x");
+	gStyle->SetTitleOffset(0.2,"y");
+	
+	gStyle->SetTitleBorderSize(0);
+	
+	
+	plot->GetXaxis()->SetTitle("Column");
+	plot->GetYaxis()->SetTitle("Row");
+	return NOERROR;
+
+	cout<< "ALL PLOTS INITIALIZED SUCCESSFULLY\n\n\n\n\n";
+
+
+	// hitsDistrubution = new TH2Poly();
+
+	return NOERROR;
+}
+
+//------------------
+// brun
+//------------------
+jerror_t JEventProcessor_pluginOfLMS::brun(JEventLoop *eventLoop, int32_t runnumber)
+{
+	// This is called whenever the run number changes
+	return NOERROR;
+}
+
+//------------------
+// evnt
+//------------------
+jerror_t JEventProcessor_pluginOfLMS::evnt(JEventLoop *loop, uint64_t eventnumber)
+{
+
+	// This is called for every event. Use of common resources like writing
+	// to a file or filling a histogram should be mutex protected. Using
+	// loop->Get(...) to get reconstructed objects (and thereby activating the
+	// reconstruction algorithm) should be done outside of any mutex lock
+	// since multiple threads may call this method at the same time.
+	// Here's an example:
+	//
+	// vector<const MyDataClass*> mydataclasses;
+	// loop->Get(mydataclasses);
+	//
+	// japp->RootFillLock(this);
+	//  ... fill historgrams or trees ...
+	// japp->RootFillUnLock(this);
+	vector<const DECALDigiHit *> ecalDigitHits;
+	vector<const DECALHit *> ecalHits;
+	loop->Get(ecalDigitHits);
+	loop->Get(ecalHits);
+	// vector<const DFCALHit *>fcalhits;
+	// loop->Get(fcalhits);
+
+
+	int nGood  = 0;
+	int nGoodD = 0;
+
+	int col, row;
+	Double_t pulse_integral, pulse_time, pulse_peak, pedestal ;
+	std::vector<bool> goodChannelEvent(ecalHits.size(), true);
+	std::vector<bool> goodChannelEventD(ecalDigitHits.size(), true);
+
+	std::vector<bool> hasNeighbor   (ecalHits.size(), false);
+	std::vector<bool> multiNeighbor (ecalHits.size(), false);
+	std::vector<bool> hasNeighborD  (ecalDigitHits.size(), false);
+	std::vector<bool> multiNeighborD(ecalDigitHits.size(), false);
+
+	// ************************* Start Cutting condition *****************************
+	// cout<<__LINE__<endl;
+	if(addCuts && ecalHits.size() >= MinEcalSizeToAccept){
+		// 假设坐标数组 coords 格式为 {{x1, y1}, {x2, y2}, ...}
+		// cut those events without neighbor
+		vector<pair<int, int>> coordinatesEcal (ecalHits.size(),{100, 100});
+		vector<pair<int, int>> coordinatesEcalD(ecalDigitHits.size(),{100, 100});
+		for(size_t i = 0; i < ecalHits.size(); i++){
+			coordinatesEcal[i].first =  ecalHits[i] -> column;
+			coordinatesEcal[i].second = ecalHits[i] -> row;
+		}
+		for(size_t i = 0; i < ecalHits.size(); i++){
+			coordinatesEcalD[i].first =  ecalDigitHits[i] -> column;
+			coordinatesEcalD[i].second = ecalDigitHits[i] -> row;
+		}
+		//  define the neighbros that we want to accept
+		vector<pair<int, int>> offsets = {
+			{-1,  3}, {0,  3}, { 1,  3},
+			{-1,  2}, {0,  2}, { 1,  2},
+			{-1,  1}, {0,  1}, { 1,  1},
+			{-1,  0},          { 1,  0}, 
+			{-1, -1}, {0, -1}, { 1, -1},
+			{-1, -2}, {0, -2}, { 1, -2},
+			{-1, -3}, {0, -3}, { 1, -3}
+		};
+		// looking for a neighbor
+		set<pair<int, int>> coords_set (coordinatesEcal.begin() , coordinatesEcal.end() );
+		set<pair<int, int>> coords_setD(coordinatesEcalD.begin(), coordinatesEcalD.end());
+        vector<pair<int, int>> result;
+		// 使用基于索引的循环遍历所有的坐标
+		for (size_t i = 0; i < coordinatesEcal.size(); ++i) {
+			// if(hasNeighbor[i]) continue;
+			int x = coordinatesEcal[i].first;
+			int y = coordinatesEcal[i].second;
+			for (auto &offset : offsets) {
+				int dx = offset.first;
+				int dy = offset.second;
+				pair<int, int> neighbor = {x + dx, y + dy};
+				auto it = coords_set.find(neighbor);
+				if (it != coords_set.end()) {
+					size_t neighborIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), neighbor));
+					hasNeighbor[i] = true;
+					hasNeighbor[neighborIndex] = true;
+					// 检查特定邻居 (x-1, y) 和 (x+1, y)
+					pair<int, int> leftNeighbor = {x - 1, y};  // (x-1, y)
+					pair<int, int> rightNeighbor = {x + 1, y};  // (x+1, y)
+
+					bool foundLeft = coords_set.find(leftNeighbor) != coords_set.end();
+					bool foundRight = coords_set.find(rightNeighbor) != coords_set.end();
+
+					// 如果都找到了，修改 multiNeighbor 标志
+					// if (foundLeft && foundRight) {
+					// 	size_t leftIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), leftNeighbor));
+					// 	size_t rightIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), rightNeighbor));
+					// 	multiNeighbor[i] = true;
+					// 	multiNeighbor[leftIndex] = true;  // 修改 (x-1, y) 对应的 multiNeighbor
+					// 	multiNeighbor[rightIndex] = true;  // 修改 (x+1, y) 对应的 multiNeighbor
+					// }
+
+					if (foundRight) {
+						size_t leftIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), leftNeighbor));
+						multiNeighbor[i] = true;
+						multiNeighbor[leftIndex] = true;  // 修改 (x-1, y) 对应的 multiNeighbor
+					}
+
+					if (foundLeft) {
+						size_t rightIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), rightNeighbor));
+						multiNeighbor[i] = true;
+						multiNeighbor[rightIndex] = true;  // 修改 (x+1, y) 对应的 multiNeighbor
+					}
+
+					break;
+				}
+			}
+		}
+
+
+		for (size_t i = 0; i < coordinatesEcalD.size(); ++i) {
+			// if(hasNeighborD[i] && multiNeighborD[i]) continue;
+			int x = coordinatesEcalD[i].first;
+			int y = coordinatesEcalD[i].second;
+			for (auto &offset : offsets) {
+				int dx = offset.first;
+				int dy = offset.second;
+				pair<int, int> neighbor = {x + dx, y + dy};
+				auto it = coords_setD.find(neighbor);
+				if (it != coords_setD.end()) {
+					size_t neighborIndex = distance(coordinatesEcalD.begin(), find(coordinatesEcalD.begin(), coordinatesEcalD.end(), neighbor));
+					hasNeighborD[i] = true;
+					hasNeighborD[neighborIndex] = true;
+					// 检查特定邻居 (x-1, y) 和 (x+1, y)
+					pair<int, int> leftNeighbor = {x - 1, y};  // (x-1, y)
+					pair<int, int> rightNeighbor = {x + 1, y};  // (x+1, y)
+
+					bool foundLeft = coords_setD.find(leftNeighbor) != coords_setD.end();
+					bool foundRight = coords_setD.find(rightNeighbor) != coords_setD.end();
+
+					// 如果都找到了，修改 multiNeighbor 标志
+					// if (foundLeft && foundRight) {
+					// 	size_t leftIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), leftNeighbor));
+					// 	size_t rightIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), rightNeighbor));
+					// 	multiNeighbor[i] = true;
+					// 	multiNeighbor[leftIndex] = true;  // 修改 (x-1, y) 对应的 multiNeighbor
+					// 	multiNeighbor[rightIndex] = true;  // 修改 (x+1, y) 对应的 multiNeighbor
+					// }
+
+					if (foundRight) {
+						size_t leftIndex = distance(coordinatesEcalD.begin(), find(coordinatesEcalD.begin(), coordinatesEcalD.end(), leftNeighbor));
+						multiNeighborD[i] = true;
+						multiNeighborD[leftIndex] = true;  // 修改 (x-1, y) 对应的 multiNeighbor
+					}
+
+					if (foundLeft) {
+						size_t rightIndex = distance(coordinatesEcal.begin(), find(coordinatesEcal.begin(), coordinatesEcal.end(), rightNeighbor));
+						multiNeighborD[i] = true;
+						multiNeighborD[rightIndex] = true;  // 修改 (x+1, y) 对应的 multiNeighbor
+					}
+					break;
+				}
+			}
+		}
+		
+
+		for(size_t i = 0; i < ecalHits.size(); i++){
+			goodChannelEvent[i] = goodChannelEvent[i] && (ecalHits[i] -> column >= 20);
+			// goodChannelEvent[i] = goodChannelEvent[i] && (ecalDigitHits[i] -> pulse_time >= cutsConstants::digiHitsPeakPosLowerLimit );
+			// goodChannelEvent[i] = goodChannelEvent[i] && (ecalDigitHits[i] -> pulse_time <= cutsConstants::digiHitsPeakPosUpperLimit);
+			goodChannelEvent[i] = goodChannelEvent[i] && (hasNeighbor[i]);
+			goodChannelEvent[i] = goodChannelEvent[i] && (!multiNeighbor[i]);
+			if(goodChannelEvent[i]) nGood ++;
+		}
+		for(size_t i = 0; i < ecalDigitHits.size(); i++){
+			goodChannelEventD[i] = goodChannelEventD[i] && (ecalDigitHits[i] -> column >= 20);
+			// goodChannelEvent[i] = goodChannelEvent[i] && (ecalDigitHits[i] -> pulse_time >= cutsConstants::digiHitsPeakPosLowerLimit );
+			// goodChannelEvent[i] = goodChannelEvent[i] && (ecalDigitHits[i] -> pulse_time <= cutsConstants::digiHitsPeakPosUpperLimit);
+			goodChannelEventD[i] = goodChannelEventD[i] && (hasNeighborD[i]);
+			goodChannelEventD[i] = goodChannelEventD[i] && (!multiNeighborD[i]);
+			if(goodChannelEventD[i]) nGoodD ++;
+		}
+	}
+
+	
+	// **************************   END cut condition  ************************************
+	// ************************************************************************************
+
+
+	// 	for(unsigned int i = 0; i < ecalDigitHits.size(); i++){
+	// 	col = fcalhits[i]->column;
+	// 	row = fcalhits[i]->row;
+	// 	// energy = fcalhits[i]->E;
+	// 	// time = fcalhits[i]->t;
+	// 	FcalEnergyRecordMatrix[col][row] += energy;
+	// 	FcalTimeRecordMatrix[col][row] += time;
+	// 	FcalHitsRecordMatrix[col][row] += 1;
+	// 	// findBigger(lookingForMaxEnergy,energy);
+	// 	// findBigger(lookingForMaxTime,time);
+	// }
+	// outputFile << '\n' << "ECAL TRACK : " ;
+
+	//for first cut, accepting col >= 20, time in time window
+	// cout << "ecalHits Size = " << ecalHits.size() << endl;
+	// cout << "ecalDigitHits Size = " << ecalDigitHits.size() << endl;
+
+
+	//   *******************        Start to Fill Tree  **********************
+	//   *******************      Ecal Hits Tree       ***********************
+	for (unsigned int i=0;i<ecalHits.size();i++){
+	//if no enough good channel in a single event, or the channels that passed cuts are not enough, discard the trigger
+		// if(ecalHits.size() < MinEcalSizeToAccept) break;
+		// if(nGood < 5) break;
+	// 	if accept the trigger, then go on
+	// 	if the channel did not pass cuts, then it is background, ignore this channel
+		// if(!goodChannelEvent[i]) continue;
+	// 	if it is signal, then fill the tree
+		col = ecalHits[i]->column;
+		row = ecalHits[i]->row;
+		channelNoByColumn      = channelMapByCol[col][row];
+		energyBranchVar        = ecalHits[i] -> E;
+		timeBranchVar          = ecalHits[i] -> t;
+		ecalHitsTree->Fill();
+		
+		numberGoodChannelEvents ++;
+		plot->Fill(col,row,energyBranchVar);
+	}
+	if(printAllAcceptedEventsAfterCuts){
+		my_canvas->cd();
+		gPad->SetGrid(1);
+		my_canvas->Update();
+		if(ecalHits.size() >= MinEcalSizeToAccept && nGood >= 5){
+			plot->Draw("zcol");
+			my_canvas->Print(TString::Format("./figures/EcalHitsEventNo_%03d.pdf", plotIndex));
+			my_canvas->Print(TString::Format("./figures/EcalHitsEventNo_%03d.png", plotIndex));
+			plotIndex ++;
+		}
+		my_canvas->Clear();
+		plot->Reset();
+	}
+
+	// Do the same with Ecal Digit Hit
+	//   ******************     ECAL Digit Hits Tree      ***************************
+	for (unsigned int i=0;i<ecalDigitHits.size();i++){
+		//if no enough good channel in a single event, or the channels that passed cuts are not enough, discard the trigger
+		// if(ecalDigitHits.size() < MinEcalSizeToAccept) break;
+		// if(nGoodD < 5) break;
+		// 	if accept the trigger, then go on
+		// 	if the channel did not pass cuts, then it is background, ignore this channel
+		// if(!goodChannelEventD[i]) continue;
+		col = ecalDigitHits[i]->column;
+		row = ecalDigitHits[i]->row;
+		// cout << "if good channel in a single event, process next step" <<endl;
+		pulseIntegralBranchVar = ecalDigitHits[i]->pulse_integral;
+		pulseTimeBranchVar     = ecalDigitHits[i]->pulse_time    ;
+		pulsePeakBranchVar     = ecalDigitHits[i]->pulse_peak - ecalDigitHits[i]->pedestal / 4.0;
+		if (pulsePeakBranchVar <= 0 ) break;
+		if(cok < 20) break;
+		digiChannelNoByColumn  = channelMapByCol[col][row];
+		if(digiChannelNoByColumn > 800){
+			cout << "(col, row) =  "<< col << " , " << row  << endl;
+			
+		}
+		ecalDigitHitsTree->Fill();
+		numberGoodChannelEvents ++;
+	}
+	// getchar();
+	//    ********************** End filling Tree   ********************************
+	if(printAllAcceptedEventsAfterCuts){
+		my_canvas->cd();
+		gPad->SetGrid(1);
+		my_canvas->Update();
+		if(ecalHits.size() >= MinEcalSizeToAccept && nGood >= 0){
+			plot->Draw("zcol");
+			my_canvas->Print(TString::Format("./figures/EcalHitsEventNo_%07d_Digit.pdf", plotIndex));
+			my_canvas->Print(TString::Format("./figures/EcalHitsEventNo_%07d_Digit.png", plotIndex));
+			plotIndex ++;
+		}
+		my_canvas->Clear();
+		plot->Reset();
+	}
+
+	// outputFile << endl << endl;
+	// outputFile << "ENEVT LOOPED = " << eventNo << endl << endl;
+	// eventNo += 1;
+
+	// cout << numberGoodChannelEvents << endl;
+	return NOERROR;
+
+}
+
+//------------------
+// erun
+//------------------
+jerror_t JEventProcessor_pluginOfLMS::erun(void)
+{
+	// This is called whenever the run number changes, before it is
+	// changed to give you a chance to clean up before processing
+	// events from the next run number.
+	return NOERROR;
+}
+
+//------------------
+// fini
+//------------------
+jerror_t JEventProcessor_pluginOfLMS::fini(void)
+{
+	// Called before program exit after event processing is finished.
+	cout << "****************************\n"<< numberGoodChannelEvents << endl;
+	cout 
+	<< endl << "Entries that passed cuts in total  =  "
+	<< numberGoodChannelEvents << endl
+	<< "****************************\n\n\n"
+	;
+
+	return NOERROR;
+}
+
---
+++ pluginOfLMS/JEventProcessor_pluginOfLMS.h
@@ -0,0 +1,173 @@
+// $Id$
+//
+//    File: JEventProcessor_pluginOfLMS.h
+// Created: Thu Nov 21 07:23:30 PM EST 2024
+// Creator: zhikun (on Linux ifarm2402.jlab.org 5.14.0-427.42.1.el9_4.x86_64 x86_64)
+//
+
+#ifndef _JEventProcessor_pluginOfLMS_
+#define _JEventProcessor_pluginOfLMS_
+
+
+#include <JANA/JEventProcessor.h>
+#include <ECAL/DECALHit.h>
+#include <ECAL/DECALDigiHit.h>
+#include <FCAL/DFCALHit.h>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "TCanvas.h"
+#include "TH2Poly.h"
+#include "TRandom.h"
+#include "TStyle.h"
+#include <TTree.h>
+#include <fstream>
+#include <fstream>
+#include "/w/halld-scshelf2101/home/zhikun/zhikunTemplates/classByZhikun.h"
+#include "/w/halld-scshelf2101/home/zhikun/zhikunTemplates/commonFunctions.h"
+#include "/w/halld-scshelf2101/home/zhikun/zhikunTemplates/zhikunConstants.h"
+#include "namespaceFucction.h"
+using namespace classByZhikun;
+using namespace std;
+namespace cutsConstants{
+	const bool addCuts = false;
+	const Double_t digiHitsPeakPosLowerLimit = 80 ;
+	const Double_t digiHitsPeakPosUpperLimit = 200;
+}
+
+using namespace std;
+
+namespace globalVariables{
+    // std::ofstream outputFile;
+    int eventLooped = 0;
+    string newestFileName;
+    plotEcalHits2D hitsCount;
+	bool showInColumn = true;
+	plotEcalHits2D hitsEnergy2D(
+			"",        " ", "EnergyDistribution2D"     , "Energy Distribution"
+		//	string xAxis , string yAxis, string name,  string title
+		);
+	plotEcalHits2D hitsTime2D(
+		"",        "", "TimeDistribution2D"     , "Time Distribution"
+	//	string xAxis , string yAxis, string name,  string title
+	);
+	plotEcalHits2D hitsCount2D(
+		"",        "", "HitsDistribution2D"     , "Hits Distribution"
+	//	string xAxis , string yAxis, string name,  string title
+	);
+
+	plotEcalHits2D rowChannelEnergy2D(
+		"Channel Number By Row", " Energy/GeV", "rowChannelEnergy2D", "Energy Distribution VS Channels by row",
+		new TH2D("rowChannelEnergy2D","Energy Distribution VS Channels by row", EcalChannelNums, 0, EcalChannelNums, energyBins, minEnergy, maxEnergy)
+	);
+	plotEcalHits2D columnChannelEnergy2D(
+		"Channel Number By Column", " Energy/GeV", "columnChannelEnergy2D", "Energy Distribution VS Channels by col",
+		new TH2D("columnChannelEnergy2D","Energy Distribution VS Channels by col", EcalChannelNums, 0, EcalChannelNums, energyBins, minEnergy, maxEnergy)
+	);
+	plotEcalHits2D rowChannelTime2D(
+		"Channel Number By Row", " Time/ns", "rowChannelTime2D", "Time Distribution VS Channels by row",
+		new TH2D("rowChannelTime2D","Time Distribution VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+	plotEcalHits2D columnChannelTime2D(
+		"Channel Number By Column", " Time/ns", "columnChannelTime2D", "Time Distribution VS Channels by col",
+		new TH2D("columnChannelTime2D","Time Distribution VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+
+	// Digital Values 2D plots variable VS channel index
+	plotEcalHits2D rowChannelDigitalPeak2D(
+		"Channel Number By Row", " Digital Pause Peak/Digital Counts", "rowChannelDigitalPeak2D", "Digital Pause Peak VS Channels by row",
+		new TH2D("rowChannelDigitalPeak2D","Digital Pause Peak VS Channels by row", EcalChannelNums, 0, EcalChannelNums, energyBins, minEnergy, maxEnergy)
+	);
+	plotEcalHits2D columnChannelDigitalPeak2D(
+		"Channel Number By Column", " Digital Pulse Peak/Digital Counts", "columnChannelDigitalPeak2D", "Digital Pause Peak VS Channels by col",
+		new TH2D("columnChannelEnergy2D","Energy Distribution VS Channels by col", EcalChannelNums, 0, EcalChannelNums, energyBins, minEnergy, maxEnergy)
+	);
+
+	plotEcalHits2D rowChannelDigitalIntegral2D(
+		"Channel Number By Row", " Pause Integral", "rowChannelDigitalIntegral2D", "Digital Integral VS Channels by row",
+		new TH2D("rowChannelDigitalIntegral2D","Digital Integral VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+	plotEcalHits2D columnChannelDigitalIntegral2D(
+		"Channel Number By Column", " Pause Integral", "rowChannelDigitalIntegral2D", "Digital Integral VS Channels by col",
+		new TH2D("columnChannelDigitalIntegral2D","Digital Integral VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+
+	plotEcalHits2D rowChannelDigitalTime2D(
+		"Channel Number By Row", " Digital Time ", "rowChannelDigitalTime2D", "Digital Time Distribution VS Channels by row",
+		new TH2D("rowChannelDigitalTime2D","Time Distribution VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+	plotEcalHits2D columnChannelDigitalTime2D(
+		"Channel Number By Column", "Digital Time/sample", "columnChannelDigitalTime2D", "Digital Time Distribution VS Channels by col",
+		new TH2D("columnChannelDigitalDigitalTime2D","Time Distribution VS Channels by row", EcalChannelNums, 0, EcalChannelNums, timeBins, 0, maxTime)
+	);
+
+
+
+	plotEcalHits1D EcalTimeColumnPlot1D = 
+	plotEcalHits1D(new TH1D("EcalTimeColumPlot1D", "Time Distribution of Ecal in column", EcalChannelNums, 0, EcalChannelNums));
+
+	plotEcalHits1D EcalEnergyColumnPlot1D = 
+	plotEcalHits1D(new TH1D("EcalEnergyColumPlot1D", "Energy Distribution of Ecal in column", EcalChannelNums, 0, EcalChannelNums)); 
+	// 初始化，需要传递 hist 指针
+
+	plotEcalHits1D FcalTimeColumnPlot1D = 
+	plotEcalHits1D(new TH1D("FcalTimeColumPlot1D", "Time Distribution of Fcal in column", FcalChannelNums, 0, FcalChannelNums));
+
+	plotEcalHits1D FcalEnergyColumnPlot1D = 
+	plotEcalHits1D(new TH1D("FcalEnergyColumPlot1D", "Energy Distribution of Fcal in column", FcalChannelNums, 0, FcalChannelNums)); 
+	// 初始化，需要传递 hist 指针
+	
+	plotEcalHits1D EcalTimeRowPlot1D = 
+	plotEcalHits1D(new TH1D("EcalTimeRowPlot1D", "Time Distribution of Ecal", EcalChannelNums, 0, EcalChannelNums));
+
+	plotEcalHits1D EcalEnergyRowPlot1D = 
+	plotEcalHits1D(new TH1D("EcalEnergyRowPlot1D", "Energy Distribution of Ecal", EcalChannelNums, 0, EcalChannelNums)); 
+
+
+	plotEcalHits1D FcalTimeRowPlot1D = 
+	plotEcalHits1D(new TH1D("FcalTimeRowPlot1D", "Time Distribution of Fcal", FcalChannelNums, 0, FcalChannelNums));
+
+	plotEcalHits1D FcalEnergyRowPlot1D = 
+	plotEcalHits1D(new TH1D("FcalEnergyRowPlot1D", "Energy Distribution of Fcal", FcalChannelNums, 0, FcalChannelNums)); 
+
+	std::vector<std::vector<Double_t>> EcalEnergyRecordMatrix(sizeOfEcal, std::vector<Double_t>(sizeOfEcal, 0));
+	std::vector<std::vector<Double_t>> EcalTimeRecordMatrix(sizeOfEcal, std::vector<Double_t>(sizeOfEcal, 0));
+	std::vector<std::vector<Double_t>> FcalEnergyRecordMatrix(sizeOfFcal, std::vector<Double_t>(sizeOfFcal, 0));
+	std::vector<std::vector<Double_t>> FcalTimeRecordMatrix(sizeOfFcal, std::vector<Double_t>(sizeOfFcal, 0));
+	std::vector<std::vector<Double_t>> EcalHitsRecordMatrix(sizeOfEcal, std::vector<Double_t>(sizeOfEcal, 0));
+	std::vector<std::vector<Double_t>> FcalHitsRecordMatrix(sizeOfFcal, std::vector<Double_t>(sizeOfFcal, 0));
+	float channelMapByRow[sizeOfEcal][sizeOfEcal] = {0,};
+	float channelMapByCol[sizeOfEcal][sizeOfEcal] = {0,};
+
+	Int_t channelNoByColumn, digiChannelNoByColumn;
+	int numberGoodChannelEvents = 0;
+	Double_t energyBranchVar, timeBranchVar, pulsePeakBranchVar, pulseIntegralBranchVar, pulseTimeBranchVar;
+	
+	TCanvas *my_canvas  = new TCanvas("", "",2400, 1600);
+	TH2D    *plot       = new TH2D   ("", "", sizeOfEcal, 0, sizeOfEcal, sizeOfEcal, 0, sizeOfEcal);
+	
+}
+
+
+class JEventProcessor_pluginOfLMS:public jana::JEventProcessor{
+	public:
+		JEventProcessor_pluginOfLMS();
+		~JEventProcessor_pluginOfLMS();
+		const char* className(void){return "JEventProcessor_pluginOfLMS";}
+		TTree* ecalHitsTree;
+		TTree* ecalDigitHitsTree;
+	
+	private:
+		jerror_t init(void);						///< Called once at program start.
+		jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
+		jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
+		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
+		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+};
+
+#endif // _JEventProcessor_pluginOfLMS_
+
